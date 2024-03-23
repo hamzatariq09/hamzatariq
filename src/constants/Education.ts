c851e815ef26a1eead6fa6c6367018e4a33f4080
hamzatariq09 <121528349+hamzatariq09@users.noreply.github.com>
2024-03-23 03:25:46 -0700

    Update Education.ts

--- src/constants/Education.ts
+++ src/constants/Education.ts
@@ -10,6 +10,11 @@ const Education: TEducation[] = [
         date: "Dec 2023",
         univeristy: "Memorial University of Newfoundland, St. John's, NL",
         subject: "Master of Applied Science in Computer Engineering"
+    },
+    {
+        date: "Jun 2021",
+        univeristy: "National University of Computer and Emerging Sciences, Islamabad",
+        subject: "Bachelor of Science in Electrical Engineering"
     }
 ]
 
